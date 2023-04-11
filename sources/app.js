44ef60ecf67f30756839f5e3a1b042e4bb0c1410
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-11 21:01:30 +0900

    modified : reset contents and add loginButton event

--- sources/app.js
+++ sources/app.js
@@ -1,17 +1,11 @@
 
-let h1 = document.querySelector("div.hello:first-child h1");
+// variables
+const   loginForm = document.querySelector("#login-form");
+// Search input, button from loginFrom
+const   loginInput = loginForm.querySelector("input");
+const   loginButton = loginForm.querySelector("button");
 
-function    handleTitleClick() {
-    const   clickedClass = "clicked";
-
-    // // Solution 1) use remove, add method
-    // if (h1.classList.contains(clickedClass))
-    //     h1.classList.remove(clickedClass);
-    // else
-    //     h1.classList.add(clickedClass);
-    
-    // Solution 2) use toggle method
-    h1.classList.toggle(clickedClass); 
-}
-
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", function() {
+    // console.dir(loginInput);
+    console.log(loginInput.value);
+});
