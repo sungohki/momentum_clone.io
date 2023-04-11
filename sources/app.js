002bb927629a71757bd998973b40fb3fb126118d
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-11 19:41:30 +0900

    modified : add conditional statement in Event Func

--- sources/app.js
+++ sources/app.js
@@ -1,46 +1,14 @@
-const   title = document.querySelector("div.hello:first-child h1");
-// queryselector : find (first) element by way of css
 
-console.log(title);
-// way to chanege property of element
-title.style.color = "blue";
+let h1 = document.querySelector("div.hello:first-child h1");
 
-// addEventListener method
-let handleTitleClick = function() {
-    console.log("title is clicked!");
+function    handleMouseEnter() {
+    const   currentColor = h1.style.color;
+    let     newcolor;
+    if(currentColor === "blue")
+        newcolor = "tomato";
+    else
+        newcolor = "blue";
+    h1.style.color = newcolor;
 }
 
-let handleMouseEnter = function() {
-    // console.log("mouse is here");
-    title.innerText = "your mouse is here";
-}
-
-let handleMouseLeave = function() {
-    title.innerText = "your mouse leaved here";
-}
-
-let handleWindowResize = function() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-let handleWindowCopy = function() {
-    alert("copy occured!!!");
-}
-
-function    handleWindowOffline() {
-    alert("WIFI off!!");
-}
-
-function    handleWindowOnline() {
-    alert("WIFI on!!!");
-}
-
-// element.addEventListener("event type", to_do);
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleMouseEnter);
