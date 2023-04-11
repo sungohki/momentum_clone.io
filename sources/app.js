f8e174fd0b56bc336b4a6ddb132ba4a32c1b1d08
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-11 20:43:24 +0900

    modified : add contents about how to control class

--- sources/app.js
+++ sources/app.js
@@ -1,14 +1,17 @@
 
 let h1 = document.querySelector("div.hello:first-child h1");
 
-function    handleMouseEnter() {
-    const   currentColor = h1.style.color;
-    let     newcolor;
-    if(currentColor === "blue")
-        newcolor = "tomato";
-    else
-        newcolor = "blue";
-    h1.style.color = newcolor;
+function    handleTitleClick() {
+    const   clickedClass = "clicked";
+
+    // // Solution 1) use remove, add method
+    // if (h1.classList.contains(clickedClass))
+    //     h1.classList.remove(clickedClass);
+    // else
+    //     h1.classList.add(clickedClass);
+    
+    // Solution 2) use toggle method
+    h1.classList.toggle(clickedClass); 
 }
 
-h1.addEventListener("click", handleMouseEnter);
+h1.addEventListener("click", handleTitleClick);
