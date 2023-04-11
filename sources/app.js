f39f74044c3c34a89fee43003a8e76aec34f2a89
sungohki <sokim1201@naver.com>
2023-04-11 16:25:07 +0900

    modified : add <h1> tag

--- sources/app.js
+++ sources/app.js
@@ -1,38 +1,7 @@
+const   title = document.getElementById("title");
 
-const calculator = {
-    add: function(a, b){
-        return (a + b);
-    },
-    minus: function(a, b){
-        return (a - b);
-    },
-    multiply: function(a, b){
-        return (a * b);
-    },
-    divide: function(a, b){
-        return (a / b);
-    },
-    powerof: function(a, b){
-        return (a ** b);
-    }
-};
+// console.dir(title);
+title.innerText = "Got you!";
 
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0)
-    // using OR Operator
-    console.log("NaN or Lower than 0 : Please write a number");
-else if (age < 18)
-    console.log("You are too young.");
-else if (age >= 18 && age <= 50)
-    // using AND Operator
-    console.log("You can drink.");
-else if (age > 50 && age <= 80)
-    // using AND Operator
-    console.log("You should exercise.");
-else if (age !== 100 && age > 80)
-    console.log("You can do whatever you want to do.");
-else if (age === 100)
-    // === Operator : type and value shouble correct
-    // if they do, it will returns true but then don't it will returns false
-    console.log("Wow, you are wise");
+console.log(title.id);
+console.log(title.className);
