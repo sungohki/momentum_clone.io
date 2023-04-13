d43a8aaec941d66140c40439f367464032adfa4f
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-13 18:08:47 +0900

    new : add directory for js files

---
+++ sources/js/background.js
@@ -0,0 +1,15 @@
+
+// variables
+const	images = [
+	"0.png",
+	"1.png",
+	"2.jpg",
+	"3.png",
+	"4.jpg",
+];
+const	chosenImage = images[Math.floor(Math.random() * (images.length))];
+
+// init
+
+
+// functions
