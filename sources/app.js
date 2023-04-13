8164a28c89c7fe9f6017e2596fdc90edc1425aaa
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-13 13:39:49 +0900

    modified : add handleLinkClick method

--- sources/app.js
+++ sources/app.js
@@ -2,15 +2,23 @@
 // variables
 const   loginForm = document.querySelector("#login-form");
 const   loginInput = document.querySelector("#login-form input");
+const   link = document.querySelector("a");
 
+// functions
 function    onLoginSubmit(event) {
     event.preventDefault();
     // preventDefault() : prevent any of Event's default behavior of brawser
-    // ex) submit ==> Default : refresh the page
     console.log(event);
     console.log(loginInput.value);
 }
 
-// submit : occur when user press or click "enter" key
+function    handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    // alert("clicked");
+}
+
+// EventListener
 loginForm.addEventListener("submit", onLoginSubmit);
-// onLoginSubmit({event info}) ==> deliver event info about what user writed
+link.addEventListener("click", handleLinkClick);
+// handleLinkClick({information about event that just happened});
